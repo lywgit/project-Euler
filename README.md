0d39883261fc63532abb3234d1b464d5bfce5b5e
lywang <lywang@tiara.sinica.edu.tw>
2017-09-13 17:34:13 +0800

    Problem 8, 9; Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# project-Euler
-Playing with project Euler at leisure 
+# Project E u l e r
+
+
+Thinking of playing [project Euler](https://projecteuler.net/) at leisure as a kind of exercise. Probably won't go far, but why not?
+
+For my own convenience, I am using this repository to save my code (backup). But note in principle, one shouldn't post solutions outside the site forum. 
+
---
+++ prob009.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 16:27:21 2017
+Problem 9
+@author: lywang
+"""
+
+
+def is_pathagorean(a, b, c):
+    assert(c>a and c>b and b>a)
+    return a**2 + b**2 == c**2
+
+def loop_triplet(triplet_sum):
+    a = 1 
+    while a <= triplet_sum:
+        b = a+1
+        while a+b <= triplet_sum:
+            c = triplet_sum - a - b
+            if c>b:
+                if is_pathagorean(a, b, c):
+                    print(a,b,c)
+                    print(a*b*c)
+                
+            b += 1
+        a += 1
+        
+
+
+# main
+    
+loop_triplet(1000)
