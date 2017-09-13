45891d30fccbe2c5468e893cdbdf7024d6b35945
lywang <lywang@tiara.sinica.edu.tw>
2017-09-13 18:20:25 +0800

    Add problem 10

---
+++ prob_010.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 17:49:14 2017
+problem 10
+@author: lywang
+"""
+
+import math
+
+def is_prime(q):
+    if q == 1:
+        return False
+    elif q == 2:
+        return True
+    else:
+        max_factor = math.ceil(math.sqrt(q))
+        for factor in range(2, max_factor+1):
+            if q % factor == 0:
+                return False
+        return True
+            
+def prime_sum(limit):
+    summation = 0
+    for i in range(1, limit):
+        if is_prime(i):
+            summation += i
+    return summation
+
+        
+# main
+
+print(prime_sum(int(2e6)))
